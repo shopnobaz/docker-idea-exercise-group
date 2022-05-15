3cc9fcd8ae4aba1f40d815d83792d32fc8e8b7cf
Ironboy <ironboy@users.noreply.github.com>
2022-05-15 02:17:58 +0200

    why not?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+console.log(process.env);
+
 export default defineConfig({
   plugins: [react()],
   server: {
