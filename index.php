be6d2c4a14fd24d1a76c95c62467acb30aefbb3b
shopnobaz <vision2025bd@yahoo.com>
2022-05-30 22:31:18 +0200

    added code to index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
  <?php
 echo "My first PHP script!";
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
 ?>
