956c9b30bd993a8e92c5b5da9cd94fe63d89bf74
shopnobaz <vision2025bd@yahoo.com>
2022-06-02 15:59:37 +0200

    added vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    // use process.env.PORT
+    // to read the environment variable
+    port: process.env.PORT
+  }
 })
